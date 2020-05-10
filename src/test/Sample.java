b2497b87d56a02ff17abb32ef57ea4a9c34c5258
ssk <sasat.s.93@gmail.com>
2020-05-10 17:00:31 +0900

    テスト合格
    参考：http://teqspaces.com/JUnit/2

---
+++ src/test/Sample.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Sample {
+	public String appendComma(String value){
+		return value + ",";
+	}
+
+}
---
+++ test/test/SampleTest.java
@@ -0,0 +1,16 @@
+// 参考：http://teqspaces.com/JUnit/2
+
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void testAppendComma(){
+		Sample sample = new Sample();
+		String retValue = sample.appendComma("abc");
+		assertEquals("abc,", retValue);
+	}
+
+}
